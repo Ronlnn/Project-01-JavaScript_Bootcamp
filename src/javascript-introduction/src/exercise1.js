102b71e541eafd7e938c0b693a7e9e7aa28ee918
guinitum <artking01kg@gmail.com>
2025-01-13 09:30:20 +0300

    feat: exercise1 done

---
+++ src/javascript-introduction/src/exercise1.js
@@ -0,0 +1,13 @@
+//Функция генерирует диапазон натуральных целых чисел
+function range(start, end) {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+console.log(range(0,5));
+console.log(range(3,3));
+console.log(range(8,0));
+console.log(range(0,0));
